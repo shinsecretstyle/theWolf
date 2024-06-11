c308023267c153d70c8bf05a28870b8ba65fe25c
Shin <574180973@qq.com>
2024-06-12 03:19:42 +0900

    一部の問題を修正

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -8,42 +8,40 @@ using UnityEngine.UI;
 public class player : MonoBehaviour
 {
     [SerializeField]
-    private int fadeID;
+    private int phaseID;
 
     public float speed;
-    public float jump;
+    public float jumpPower;
     public float jumpCD = 1f;
     float move;
-    public float fadeLimit = 5f;
-    public float fadeProcess = 0f;
+    public float phaseLimit = 5f;
+    public float phaseProcess = 0f;
     public bool canJump = true;
     //public LayerMask lightDectect;
     public bool isInLight;
-    public Slider fadeSlider;
-    public Sprite fade1;
-    public Sprite fade2;
-    public Sprite fade3;
-    public Sprite fade4;
+    public Slider phaseSlider;
+    public Sprite phase1;
+    public Sprite phase2;
+    public Sprite phase3;
+    public Sprite phase4;
     SpriteRenderer sr;
     Rigidbody2D rb;
-    PolygonCollider2D polygonCollider;
-  
-
+    PolygonCollider2D polygonCollider;  
 
     void Start()
     {
-        fadeID = 1;
-        fadeSlider.maxValue = fadeLimit;
+        phaseID = 1;
+        phaseSlider.maxValue = phaseLimit;
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         polygonCollider = GetComponent<PolygonCollider2D>();
         speed = 4;
-        jump = 42;
+        jumpPower = 42;
     }
 
     void Update()
     {
-        fadeSlider.value = fadeProcess;
+        phaseSlider.value = phaseProcess;
         move = Input.GetAxis("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector2(move * speed,rb.velocity.y);
@@ -53,46 +51,46 @@ public class player : MonoBehaviour
         {
             canJump = false;
             //ジャンプの高さを維持するため、mass掛け算
-            rb.AddForce(new Vector2 (rb.velocity.x,jump * 10 * rb.mass));
+            rb.AddForce(new Vector2 (rb.velocity.x,jumpPower * 10 * rb.mass));
             StartCoroutine(resetJumpCD());
         }
 
         inTheLight();
     }
 
-    //fadeによって属性値の設定
-    private void setFadeByID(int id)
+    //phaseによって属性値の設定
+    private void setPhaseByID(int id)
     {
         
-        if(id == 1)//fade1
+        if(id == 1)//phase1
         {
-            sr.sprite = fade1;
+            sr.sprite = phase1;
             updateCollider();
             speed = 4;
             rb.mass = 1;
         }
-        else if(id == 2)//fade2
+        else if(id == 2)//phase2
         {
             speed = 6;
-            sr.sprite = fade2;
+            sr.sprite = phase2;
             updateCollider();
             rb.mass = 1;
         }
-        else if(id == 3)//fade3
+        else if(id == 3)//phase3
         {
-            sr.sprite = fade3;
+            sr.sprite = phase3;
             updateCollider();
             //massを増加し、車を押すことができる
             rb.mass = 5f;
         }
-        else if(id == 4)//fade4
+        else if(id == 4)//phase4
         {
-            sr.sprite = fade4;
+            sr.sprite = phase4;
             updateCollider();
         }
     }
 
-    //fadeによってCollierを更新する
+    //phaseによってCollierを更新する
     private void updateCollider()
     {
         int pathCount = sr.sprite.GetPhysicsShapeCount();
@@ -110,35 +108,40 @@ public class player : MonoBehaviour
     {
         if (isInLight)
         {
-            fadeProcess += Time.deltaTime * 0.8f;
+            phaseProcess += Time.deltaTime * 0.8f;
         }
-        else if (!isInLight && fadeProcess > 0f)
+        else if (!isInLight && phaseProcess > 0f)
         {
-            fadeProcess -= Time.deltaTime * 0.2f;
+            phaseProcess -= Time.deltaTime * 0.2f;
         }
 
-        if (fadeProcess >= fadeLimit && fadeID < 4)
+        if (phaseProcess >= phaseLimit && phaseID < 4)
         {
-            fadeID++;
-            setFadeByID(fadeID);
-            fadeProcess = 0f;
+            phaseID++;
+            setPhaseByID(phaseID);
+            phaseProcess = 0f;
         }
-        else if (fadeProcess <= 0 && fadeID > 1) {
-            fadeID--;
-            setFadeByID(fadeID);
-            fadeProcess = fadeLimit;
+        else if (phaseProcess <= 0 && phaseID > 1) {
+            phaseID--;
+            setPhaseByID(phaseID);
+            phaseProcess = phaseLimit;
         }
-        else if (fadeProcess <= 0 && fadeID == 1)
+        else if (phaseProcess <= 0 && phaseID == 1)
         {
-            fadeProcess = 0f;
+            phaseProcess = 0f;
         }
 
     }
 
+    private void Jump()
+    {
+
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Ground")) {
             rb.velocity = Vector2.zero;
+            canJump = true;
         }
     }
     
