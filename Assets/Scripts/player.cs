b1666f60d476a488c44c78bf9271927da7fe6a47
Shin <574180973@qq.com>
2024-06-11 19:41:21 +0900

    fade変更を追加
    fadeによって属性値の設定、Colliderの更新。

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 
 public class player : MonoBehaviour
 {
+    [SerializeField]
+    private int fadeID;
+
     public float speed;
     public float jump;
     public float jumpCD = 1f;
@@ -19,24 +22,21 @@ public class player : MonoBehaviour
     public Slider fadeSlider;
     public Sprite fade1;
     public Sprite fade2;
-
+    public Sprite fade3;
+    public Sprite fade4;
     SpriteRenderer sr;
-    //public GameObject headLeft;
-    //public GameObject headRight;
-    //public GameObject footLeft;
-    //public GameObject footRight;
-    //public GameObject moon;
-
-
     Rigidbody2D rb;
+    PolygonCollider2D polygonCollider;
   
 
 
     void Start()
     {
+        fadeID = 1;
         fadeSlider.maxValue = fadeLimit;
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        polygonCollider = GetComponent<PolygonCollider2D>();
         speed = 4;
         jump = 42;
     }
@@ -48,20 +48,6 @@ public class player : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector2(move * speed,rb.velocity.y);
         //rb.velocity = new Vector2(rb.velocity.x,y*2);
-    
-
-        if(isInLight)
-        {
-            fadeProcess += Time.deltaTime * 0.8f;
-        }else if(!isInLight && fadeProcess > 0f)fadeProcess -= Time.deltaTime * 0.2f;
-
-        if(fadeProcess >= fadeLimit)
-        {
-            sr.sprite = fade2;
-            //massを増加し、車を押すことができる
-            rb.mass = 5f;
-            fadeProcess = 0f;
-        }
 
         if (Input.GetButton("Jump") && canJump)
         {
@@ -71,7 +57,84 @@ public class player : MonoBehaviour
             StartCoroutine(resetJumpCD());
         }
 
+        inTheLight();
     }
+
+    //fadeによって属性値の設定
+    private void setFadeByID(int id)
+    {
+        
+        if(id == 1)//fade1
+        {
+            sr.sprite = fade1;
+            updateCollider();
+            speed = 4;
+            rb.mass = 1;
+        }
+        else if(id == 2)//fade2
+        {
+            speed = 6;
+            sr.sprite = fade2;
+            updateCollider();
+            rb.mass = 1;
+        }
+        else if(id == 3)//fade3
+        {
+            sr.sprite = fade3;
+            updateCollider();
+            //massを増加し、車を押すことができる
+            rb.mass = 5f;
+        }
+        else if(id == 4)//fade4
+        {
+            sr.sprite = fade4;
+            updateCollider();
+        }
+    }
+
+    //fadeによってCollierを更新する
+    private void updateCollider()
+    {
+        int pathCount = sr.sprite.GetPhysicsShapeCount();
+        polygonCollider.pathCount = pathCount;
+
+        for (int i = 0; i < pathCount; i++)
+        {
+            List<Vector2> path = new List<Vector2>();
+            sr.sprite.GetPhysicsShape(i, path);
+            polygonCollider.SetPath(i, path.ToArray());
+        }
+    }
+
+    private void inTheLight()
+    {
+        if (isInLight)
+        {
+            fadeProcess += Time.deltaTime * 0.8f;
+        }
+        else if (!isInLight && fadeProcess > 0f)
+        {
+            fadeProcess -= Time.deltaTime * 0.2f;
+        }
+
+        if (fadeProcess >= fadeLimit && fadeID < 4)
+        {
+            fadeID++;
+            setFadeByID(fadeID);
+            fadeProcess = 0f;
+        }
+        else if (fadeProcess <= 0 && fadeID > 1) {
+            fadeID--;
+            setFadeByID(fadeID);
+            fadeProcess = fadeLimit;
+        }
+        else if (fadeProcess <= 0 && fadeID == 1)
+        {
+            fadeProcess = 0f;
+        }
+
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Ground")) {
