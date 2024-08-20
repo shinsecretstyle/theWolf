d965f7cb45bda4ce20271dd9e261fcda8381b692
kaiten924 <32210030@b.shobi-u.ac.jp>
2024-08-20 23:38:54 +0900

    時間制限追加
    参考サイトhttps://zenn.dev/fujimiya/articles/5775dd0824031d

---
+++ Assets/Scripts/TimeCounter.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeCounter : MonoBehaviour
+{
+    public int countdownMinutes = 3;
+    private float countdownSeconds;
+    private Text timeText;
+
+    private void Start()
+    {
+        timeText = GetComponent<Text>();
+        countdownSeconds = countdownMinutes * 60;
+    }
+
+    void Update()
+    {
+        countdownSeconds -= Time.deltaTime;
+        var span = new TimeSpan(0,0,(int)countdownSeconds);
+        timeText.text =span.ToString(@"mm\:ss");
+
+        if(countdownSeconds <= 0)
+        {
+            SceneManager.LoadScene("Gameover");
+        }
+    }
+    
+
+}
