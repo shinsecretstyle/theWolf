28e2d904c87cfbb611522b6c1adcba234e81a3ab
oshioniya <32210041@b.shobi-u.ac.jp>
2024-09-22 19:32:51 +0900

    wolf mapシーンに導入

--- Assets/Scripts/Replay.cs
+++ Assets/Scripts/Replay.cs
@@ -5,7 +5,8 @@ public class Replay : MonoBehaviour
 {
     public void replay()
     {
-        // LoadSceneの引数にシーンの名前を入れて読み込む
-        SceneManager.LoadScene("WolfMap2(アニメーションあり)");
+            // LoadSceneの引数にシーンの名前を入れて読み込む
+            SceneManager.LoadScene("WolfMap2(アニメーションあり)");
     }
+            
 }
