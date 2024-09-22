0b1ffbd75a220e561bf770ff82423ba52b2feb8c
oshioniya <32210041@b.shobi-u.ac.jp>
2024-09-22 12:22:40 +0900

    RestartとReplayの追加

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+
+}
---
+++ Assets/Scripts/Replay.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Replay : MonoBehaviour
+{
+    public void replay()
+    {
+        // LoadSceneの引数にシーンの名前を入れて読み込む
+        SceneManager.LoadScene("WolfMap2(アニメーションあり)");
+    }
+}
